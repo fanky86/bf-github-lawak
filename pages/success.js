48deeeebbdb4cafd803d87ff6d2971b37362eddf
MR.ExceFaN <106883384+fanky86@users.noreply.github.com>
2025-01-06 19:45:15 +0700

    Create success.js

---
+++ pages/success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Pembayaran Berhasil</h1>
+      <p>Terima kasih telah melakukan pembayaran.</p>
+    </div>
+  );
+}
